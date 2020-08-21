a31bc7b5f6446b57336c41d5ddea6d5f943f1020
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-08-21 12:51:52 -0400

    All the class, interfaces completed. Test done using Postman

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,13 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
@@ -15,4 +17,10 @@ public class FakePersonDataAccessService implements PersonDao {
         DB.add(new Person(id, person.getName()));
         return 1;
     }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
 }
